04ea2ae118149a0d6ff8f9e12b6a11c13f1f0a84
yoyogold-a11 <tmrfack@gmail.com>
2021-11-12 19:23:29 -0500

    routes

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBu-s7R0lNyQHDXOMA9yBcAc2NFE17iQbo",
+    authDomain: "clone-ce166.firebaseapp.com",
+    projectId: "clone-ce166",
+    storageBucket: "clone-ce166.appspot.com",
+    messagingSenderId: "1051154686050",
+    appId: "1:1051154686050:web:0a405470a1da9898ed313b",
+    measurementId: "G-M9HG7S49E0"
+  };
